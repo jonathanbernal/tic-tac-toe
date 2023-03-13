9ca30c242ad5798305fbb7d0ea6388a3d234dae2
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2023-03-13 15:46:31 -0400

    Implement gameboard logic and unit tests.

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -3,6 +3,8 @@ import Cell from './Cell';
 const Gameboard = function () {
     const ROW_SIZE = 3;
     const COLUMN_SIZE = 3;
+    const PLAYER_MARK_PLACEMENT_SUCCESS = 0;
+    const PLAYER_MARK_PLACEMENT_ERROR = -1;
     let board = [];
 
     const positionMap = {
@@ -26,28 +28,50 @@ const Gameboard = function () {
         }
     };
 
-    const getBoard = () => board;
+    /**
+     * This function checks if the cell at the given position does not have
+     * a mark stored in it.
+     * @param {*} cellPosition the position between 1 and 9 to check.
+     * @returns whether the cell at the indicated position is empty or not.
+     */
+    const isCellAtPositionEmpty = (cellPosition) => {
+        let mark = board[positionMap[cellPosition].x][positionMap[cellPosition].y].getMark();
+        return mark === undefined;
+    }
 
-    /*
-       To make it easier for the player to remember the positions,
-       we ask the player for a position between 1 and 9 and we
-       take it from there.
-    */
-    const placePlayerMark = (player, cellPosition) => {
-        board[positionMap[1].x][positionMap[1].y].addMark(player.getMark());
+    /**
+     * 
+     * @param {*} playerMark the player's assigned mark
+     * @param {*} cellPosition A position value between 1 and 9 on the board to place the mark on.
+     * @returns the operation status. 0 if successful, -1 otherwise
+     */
+    const placePlayerMark = (playerMark, cellPosition) => {
+        if (isCellAtPositionEmpty(cellPosition)) {
+            board[positionMap[cellPosition].x][positionMap[cellPosition].y].addMark(playerMark);
+            return PLAYER_MARK_PLACEMENT_SUCCESS;
+        }
+        return PLAYER_MARK_PLACEMENT_ERROR;
     };
 
     const printBoard = () => {
-        const boardString = ""
+        let boardString = ""
         for(let row = 0; row < ROW_SIZE; row += 1) {
-            board[row].map((cell) => boardString += cell.getMark());
+            board[row].map((cell) => boardString += " " + cell.getMark());
             boardString += "\n";
         };
 
         console.log(boardString);
     }
 
-    return {getBoard, placePlayerMark, printBoard};
+    initializeBoard();
+    const getBoard = () => board;
+
+    return {
+        getBoard,
+        placePlayerMark,
+        printBoard,
+        isCellAtPositionEmpty,
+    };
 }
 
-export {Gameboard};
+export default Gameboard;
---
+++ tests/Gameboard.test.js
@@ -0,0 +1,40 @@
+import Gameboard from '../src/Gameboard.js';
+import Player from '../src/Player.js';
+
+describe('Tests for the Gameboard object', ()=>{
+    // initialize Gameboard
+    const gameboard = Gameboard();
+
+    test('Gameboard should create a 9x9 grid', ()=>{
+        expect(gameboard.getBoard().reduce((accumulator, row)=>accumulator + row.length, 0)).toBe(9);
+    });
+    test('Placing a mark in a cell should contain the mark', ()=>{
+        const testPlayer = Player('Player1', 'X');
+        // The gameboard contains a private map that maps single number positions
+        // to 2D array coordinates in the form (x,y).
+        const positionToPlaceMarkIn = 5;
+        gameboard.placePlayerMark(testPlayer.getMark(), positionToPlaceMarkIn);
+        expect(gameboard.getBoard()[1][1].getMark()).toBe('X');
+    });
+    test('Placing a mark on the 4th position should be successful', ()=>{
+        const testPlayer = Player('Player3', 'O');
+        const positionToPlaceMarkIn = 4;
+        expect(gameboard.placePlayerMark(testPlayer.getMark(), positionToPlaceMarkIn))
+            .toEqual(0);
+    });
+    test('Placing another mark again on the 4th position should not be successful', ()=>{
+        const testPlayer = Player('Player1', 'X');
+        const positionToPlaceMarkIn = 4;
+        expect(gameboard.placePlayerMark(testPlayer.getMark(), positionToPlaceMarkIn))
+            .toEqual(-1);
+    });
+    test('The argument cell should be empty (1-9)', ()=>{
+        expect(gameboard.isCellAtPositionEmpty(1)).toBe(true);
+    });
+    test('The last cell should be empty', ()=>{
+        expect(gameboard.isCellAtPositionEmpty(9)).toBe(true);
+    });
+    test('5th cell should not be empty from previous test', ()=>{
+        expect(gameboard.isCellAtPositionEmpty(5)).toBe(false);
+    });
+});
