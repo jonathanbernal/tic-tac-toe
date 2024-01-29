58ef164437786eb4c34b7bdca3b2fc90a25b29c1
Jonathan Bernal <16311694+jonathanbernal@users.noreply.github.com>
2024-01-28 23:53:56 -0500

    Include GameController in main?

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import GameController from './components/GameController'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GameController />
   </React.StrictMode>,
 )
